f443258ee8eb38c27816d54fe76f5c4efa789c43
melisdogan <melis@melisdogan.com>
2019-12-31 22:50:21 +0300

    TODO reduce redundancy

---
+++ HWClient/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class StreamTcpClient
+{
+   public static void Main()
+   {
+      string input;
+      IPEndPoint ipep = new IPEndPoint(
+                      IPAddress.Parse("127.0.0.1"), 9050);
+
+      Socket server = new Socket(AddressFamily.InterNetwork,
+                     SocketType.Stream, ProtocolType.Tcp);
+
+      try
+      {
+         server.Connect(ipep);
+      } catch (SocketException e)
+      {
+         Console.WriteLine("Unable to connect to server.");
+         Console.WriteLine(e.ToString());
+         return;
+      }
+
+
+      NetworkStream ns = new NetworkStream(server);
+      //StreamReader sr = new StreamReader(ns);
+      //StreamWriter sw = new StreamWriter(ns);
+
+   
+      Console.WriteLine(readMessageBoundary(ns));
+
+      while(true)
+      {
+         input = Console.ReadLine();
+         if (input == "exit")
+            break;
+         writeMessageBoundary(ns, input);
+         //sw.Flush();
+
+         
+         Console.WriteLine(readMessageBoundary(ns));
+      }
+      Console.WriteLine("Disconnecting from server...");
+      ns.Close();
+      server.Shutdown(SocketShutdown.Both);
+      server.Close();
+   } 
+   private static String readMessageBoundary(NetworkStream ns){
+      string next;
+      string message = "";
+      string temp = "";
+      byte[] data = new byte[1];
+      while(true){
+         data[0] = (byte)ns.ReadByte();
+         next = Encoding.ASCII.GetString(data);
+         if (next.Equals("<")) {
+            temp += next;
+            for (int i = 0; i < 6; i++){
+               data[0] = (byte)ns.ReadByte();
+               next = Encoding.ASCII.GetString(data);
+               temp += next;
+            }
+            if (temp.Equals("<##!##>")){
+               return message;
+            }
+            else message += temp;
+         }
+         else message += next;
+      }
+   }
+   private static void writeMessageBoundary(NetworkStream ns, String message){
+      byte[] data = new byte[1024];
+      message += "<##!##>";
+      data = Encoding.ASCII.GetBytes(message);
+      for (int i = 0; i < data.Length; i++ )
+         ns.WriteByte(data[i]);
+   }
+}
---
+++ HWServer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class StreamTcpSrvr
+{
+   public static void Main()
+   {
+      string data;
+      IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+
+      Socket newsock = new Socket(AddressFamily.InterNetwork,
+                      SocketType.Stream, ProtocolType.Tcp);
+
+      newsock.Bind(ipep);
+      newsock.Listen(10);
+      Console.WriteLine("Waiting for a client...");
+
+      Socket client = newsock.Accept();
+      IPEndPoint newclient = (IPEndPoint)client.RemoteEndPoint;
+      Console.WriteLine("Connected with {0} at port {1}",
+                      newclient.Address, newclient.Port);
+
+      NetworkStream ns = new NetworkStream(client);
+      //StreamReader sr = new StreamReader(ns);
+      //StreamWriter sw = new StreamWriter(ns);
+
+      string welcome = "Welcome to my test server";
+      writeMessageBoundary(ns, welcome);
+      //sw.Flush();
+
+      while(true)
+      {
+         try
+         {
+            data = readMessageBoundary(ns);
+         } catch (IOException)
+         {
+            break;
+         }
+       
+         Console.WriteLine(data);
+         writeMessageBoundary(ns, data);
+         //sw.Flush();
+      }
+      Console.WriteLine("Disconnected from {0}", newclient.Address);
+      //sw.Close();
+      //sr.Close();
+      ns.Close();
+   }
+   
+private static String readMessageBoundary(NetworkStream ns){
+      string next;
+      string message = "";
+      string temp = "";
+      byte[] data = new byte[1];
+      while(true){
+         data[0] = (byte)ns.ReadByte();
+         next = Encoding.ASCII.GetString(data);
+         if (next.Equals("<")) {
+            temp += next;
+            for (int i = 0; i < 6; i++){
+               data[0] = (byte)ns.ReadByte();
+               next = Encoding.ASCII.GetString(data);
+               temp += next;
+            }
+            if (temp.Equals("<##!##>")){
+               return message;
+            }
+            else message += temp;
+         }
+         else message += next;
+      }
+   }
+   private static void writeMessageBoundary(NetworkStream ns, String message){
+      byte[] data = new byte[1024];
+      message += "<##!##>";
+      data = Encoding.ASCII.GetBytes(message);
+      for (int i = 0; i < data.Length; i++ )
+         ns.WriteByte(data[i]);
+   }
+}
